2e071af2d780696d17f4442b6374e726c91bcf7c
Omer <ok243@live.mdx.ac.uk>
2023-06-15 09:48:13 +0100

    Add Room.java entity

---
+++ src/main/java/entity/mysql/beacon_localisation/Room.java
@@ -0,0 +1,45 @@
+	package entity.mysql.beacon_localisation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "room")
+public class Room {
+	
+	@Id
+    @Column(name = "id")
+    private int id;
+	 
+	@Column(name = "name")
+	private String name;
+	
+	public Room() {
+	
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [id=" + id + ", name=" + name + "]";
+	}
+}
