808b9021ca05faeea2aebc144b73c622b20a0b8c
EIS <ok243@live.mdx.ac.uk>
2023-07-20 17:31:12 +0100

    refactor(core): backup for mac

--- src/main/java/middleware/MyDatabaseTrigger.java
+++ src/main/java/middleware/MyDatabaseTrigger.java
@@ -1,5 +1,20 @@
 package middleware;
 
-public class MyDatabaseTrigger {
-	
+import java.util.ArrayList;
+
+import gui.MainMenu;
+
+public class MyDatabaseTrigger extends Thread {
+	private ArrayList<String> MySqlDbNames;
+	private ArrayList<String> MySqlTableNames;
+	private ArrayList<String> postgresqlDbNames;
+	private ArrayList<String> postgresqlTableNames;
+	public MyDatabaseTrigger() {
+		MainMenu.mainDbManager.getDatabaseNames();
+	}
+
+	@Override
+	public void run() {
+		
+	}
 }
