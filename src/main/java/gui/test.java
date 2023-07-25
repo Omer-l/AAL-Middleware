83f2268bf4dc6028ab9685e95e9e650a52310489
EIS <ok243@live.mdx.ac.uk>
2023-07-25 15:15:20 +0100

    add insert to postgresql db query

--- src/main/java/gui/test.java
+++ src/main/java/gui/test.java
@@ -25,7 +25,7 @@ public class test {
             String username = "root";
             String password = "root";
     		Connection connection = DriverManager.getConnection(dbUrl, username, password);
-            String sqlQuery = "SELECT record.dateTime, beacon.location, user.name"
+            String query = "SELECT record.dateTime, beacon.location, user.name"
     				+ " FROM"
     				+ 	" record"
     				+ " JOIN"
@@ -34,7 +34,7 @@ public class test {
     				+ 	" user on user.idUser = record.idUser"
     				+ " ORDER BY 1 DESC LIMIT 1";
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            ResultSet resultSet = statement.executeQuery(query);
             if (resultSet.next()) {
                 // Retrieve data from the latest row
                 latestName = resultSet.getString("name");
@@ -43,50 +43,47 @@ public class test {
                 System.out.println("Table is empty.");
             }
             String fileName = "./lastAccessedBLE.txt";
-
-//            	get the user and location
-//				read file if exists get map of userAndRoom
-            	//READ FILE
-                File f = new File ("./lastAccessedBLE.txt");
-            	if(!f.exists())
-            		f.createNewFile();
-            	BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    String[] lineSplit = line.split(",");
-                    if(lineSplit.length == 2)
-                    	nameAndLocationArr.put(lineSplit[0], lineSplit[1]);
-                }
-                bufferedReader.close();
-//            	if dbRoom and fileUserRoom matches
-//            		then do nothing to file
-//            	else
-//            		edit userAndRoom map
-//            	connect to postgresql jdbc
-        		connection.close();
-            	dbUrl = "jdbc:postgresql://localhost:5432/sensors";
-                username = "postgres";
-                password = "123456";
-                connection = DriverManager.getConnection(dbUrl, username, password);
-//            	send relevant data to incoming_events postgresql
-                
-                //write to file of the new location of user
-                // Data to save
-                StringBuilder dataToSave = new StringBuilder();
-                for(String key : nameAndLocationArr.keySet()) {
-                	dataToSave.append(key);
-                	dataToSave.append(",");
-                	dataToSave.append(nameAndLocationArr.get(key));
-                	dataToSave.append("\n");
-                }
-                System.out.println(dataToSave);
-            	FileWriter fileWriter = new FileWriter(fileName);
-                fileWriter.write(dataToSave.toString());
-                System.out.println("Data saved successfully.");
-                fileWriter.close();
-
-            } catch (IOException | SQLException e) {
-                e.printStackTrace();
+            File f = new File ("./lastAccessedBLE.txt");
+        	if(!f.exists())
+        		f.createNewFile();
+        	BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] lineSplit = line.split(",");
+                if(lineSplit.length == 2)
+                	nameAndLocationArr.put(lineSplit[0], lineSplit[1]);
             }
+            bufferedReader.close();
+    		connection.close(); //loading data complete
+            String previousRoom = nameAndLocationArr.get(latestName);
+            if(latestLocation != previousRoom)
+            	nameAndLocationArr.replace(latestName, latestLocation);
+//            	connect to postgresql jdbc
+        	dbUrl = "jdbc:postgresql://localhost:5432/sensors";
+            username = "postgres";
+            password = "123456";
+            connection = DriverManager.getConnection(dbUrl, username, password);
+//            	send relevant data to incoming_events postgresql, if user was in a different room before, send a second command confirming that previosu room is now false
+            query = "INSERT INTO public.incoming_events (state,value) VALUES ('" + latestName + "In" + latestLocation + "',true);";
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            //write to file of the new location of user
+            // Data to save
+            StringBuilder dataToSave = new StringBuilder();
+            for(String key : nameAndLocationArr.keySet()) {
+            	dataToSave.append(key);
+            	dataToSave.append(",");
+            	dataToSave.append(nameAndLocationArr.get(key));
+            	dataToSave.append("\n");
+            }
+            System.out.println(dataToSave);
+        	FileWriter fileWriter = new FileWriter(fileName);
+            fileWriter.write(dataToSave.toString());
+            System.out.println("Data saved successfully.");
+            fileWriter.close();
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
