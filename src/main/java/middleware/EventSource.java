0da516d93da1f3202f4f43708cb21ad7de3ae78a
Omer <ok243@live.mdx.ac.uk>
2023-07-31 11:02:45 +0100

    add dbmanager to branch

--- src/main/java/middleware/EventSource.java
+++ src/main/java/middleware/EventSource.java
@@ -1,14 +1,30 @@
 package middleware;
 
+import dao.MySqlConnection;
+import gui.MainMenu;
+
 public class EventSource extends Thread{
 	private EventDispatcher eventDispatcher = new EventDispatcher();
-	
-	public EventSource() {
+    private boolean listening;		
+    private MySqlConnection connection;
+
+	public EventSource(MySqlConnection connection) {
 		// TODO Auto-generated constructor stub
 	}
 
+    public void stopListening() {
+        this.listening = false;
+    }
+
 	@Override
 	public void run() {
-		
+    	//initialise prevResults
+        while (listening) {
+        	
+        }
+	}
+	
+	public static void main(String[] args) {
+        EventSource eventSource = new EventSource(MainMenu.mainDbManager);
 	}
 }
