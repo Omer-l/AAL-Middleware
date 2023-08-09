e5fe86eeb71f4ec1c931a5fa8d5750fb999b122a
EIS <ok243@live.mdx.ac.uk>
2023-08-09 17:22:29 +0100

    add test function settings.java

--- src/main/java/gui/MainMenu.java
+++ src/main/java/gui/MainMenu.java
@@ -47,8 +47,10 @@ public class MainMenu extends Application {
 	//MAIN
 	public static Stage primaryStage;
     public static void main(String[] args) {
-    	
-    		open();
+    	Settings s = new Settings();
+		String dbTb = "beacon_localisation,record\nmiddleware,schedule";
+		System.out.print(s.parseDbTb(dbTb));
+//    		open();
 //    		Rules r = new Rules();
 //    		r.open();
 //    		AddFileEvent adr = new AddFileEvent(new FileEvents(null), "run");
@@ -146,6 +148,7 @@ public class MainMenu extends Application {
     	VBox settingsVBox = new VBox(2);
     	Text settingsHeader = new Text("Settings");
     	settingsHeader.setStyle(HEADER_1_STYLE);
+    	settingsVBox.setOnMouseClicked(event -> { new Settings(null).open();});
     	VBox settingsVBox1 = new VBox(2);
     	settingsVBox1.setStyle(MENU_BUTTON_STYLE);
     	Text settingsVBox1Header = new Text("Database");
